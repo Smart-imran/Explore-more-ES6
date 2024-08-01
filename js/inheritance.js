89373127ffd81d7c51b10fe071c534c4b334f554
Smart-imran <rst.imran4@gmail.com>
2024-08-02 00:55:26 +0600

    inhritance-2

--- js/inheritance.js
+++ js/inheritance.js
@@ -1,1 +1,27 @@
 // inheritance
+
+class Vehicle{
+    constructor(name,price) {
+        this.name = name;
+        this.price = price;        
+    }
+
+    move(){
+        console.log("hello World");
+    }
+}
+
+class Bus extends Vehicle{
+    constructor(name, price ,seat , ticketPrice) {
+        super(name,price);
+        this.seat = seat ;
+        this.ticketPrice = ticketPrice;
+    }
+}
+
+class Others extends Vehicle{
+    constructor(name,price,model){
+        super(name,price);        
+        this.model = model;
+    }
+}
