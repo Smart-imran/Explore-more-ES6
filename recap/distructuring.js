05083a4e1a1676f5cfc8e4b081d5d946dc687b39
Smart-imran <rst.imran4@gmail.com>
2024-08-03 01:47:42 +0600

    recap

---
+++ recap/distructuring.js
@@ -0,0 +1,20 @@
+const numbers = [32, 2, 3, 1, 4,];
+
+const [first, secound] = numbers;
+
+//console.log(first,secound);
+
+const person = {
+    name: "Imran",
+    age: 22,
+    city: "Dhaka"
+};
+
+ const {name,age, city} = person ;
+
+ console.log(name);
+ console.log(age);
+ console.log(city);
+
+
+
---
+++ recap/spreadOperator.js
@@ -0,0 +1,13 @@
+const numbers = [32, 2, 3, 1, 4,];
+
+let findMax = Math.max(...numbers);
+ //console.log(findMax);
+
+ //spread operator to copy
+
+ const friends = [32, 2, 54, 6, 4,];
+
+ const bondhu = friends;
+bondhu.push(99);
+console.log(friends);
+console.log(bondhu);
